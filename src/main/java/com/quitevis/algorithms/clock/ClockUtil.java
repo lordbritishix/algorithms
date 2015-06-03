e1b01a8b4cecd8c4a91b98f3fde5af509811efe5
Jim Quitevis <jim.quitevis@gmail.com>
2015-06-02 23:21:58 -0700

    Updated docs

--- src/main/java/com/quitevis/algorithms/clock/ClockUtil.java
+++ src/main/java/com/quitevis/algorithms/clock/ClockUtil.java
@@ -5,15 +5,18 @@ package com.quitevis.algorithms.clock;
  */
 public class ClockUtil {
     /**
-     * abs(Degree from reference point to hour hand - Degree from reference point to minute hand) =
-     * degree between hour and minute hand
+     * abs(Degree from 12 o'clock to hour hand - Degree from 12 o'clock to minute hand) =
+     * degree between hour and minute hand, on a minute granularity
      */
     public int getAngle(int hour, int min) {
-        //Hour hand turns 360 in 720 minutes or 1/2 degree per minute
-        //degree of hour hand = 1/2 * (hour, converted in minute + minute)
+        //Hour hand turns 360 in 720 minutes or 1/2 degree per minute / 30 degrees per hour
+        //number of 30 degree angle + the remaining angle formed by the hour hand past the hour
+        //which depends on where the minute hand is at the moment
+        //
+        //degree of hour hand = 30h * 0.5m ---> 0.5(60h + m)
         int degreeOfHourHand = (int) (0.5 * (60 * hour + min));
 
-        //Minute hand turns 360 in 60 minutes or 6 degree per minute
+        //Minute hand turns 360 in 60 minutes or 6 degrees per minute
         //degree of minute hand = 6 * min
         int degreeOfMinuteHand = 6 * min;
 
